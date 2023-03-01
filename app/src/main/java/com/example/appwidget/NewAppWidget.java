6fe21e088cc3ce5abf87eb52c18489fbd3501eb2
Zafnakhairunnisa <zafna.khairunnisa@mail.ug.ac.id>
2023-03-01 14:26:07 +0700

    Initial Commit

--- app/src/main/java/com/example/appwidget/NewAppWidget.java
+++ app/src/main/java/com/example/appwidget/NewAppWidget.java
@@ -1,8 +1,10 @@
 package com.example.appwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
@@ -35,6 +37,14 @@ public class NewAppWidget extends AppWidgetProvider {
 
         String dataString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
         views.setTextViewText(R.id.appwidget_update, dataString);
+
+        Intent intentUpdate = new Intent(context, NewAppWidget.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        PendingIntent pendingIntent =PendingIntent.getBroadcast(context, appWidgetId, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+        views.setOnClickPendingIntent(R.id.buttn_update, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
